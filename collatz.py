76104ae00d545e66b9c65713f75dc62c3d439028
Administrator <zivi@plan-g.at>
2023-04-27 15:42:32 +0200

    collatz changed

--- collatz.py
+++ collatz.py
@@ -43,24 +43,46 @@ def collatz_sequence(n):
     return CollatzResponse(number, sequence, length)
 
 if __name__ == "__main__":
+
+    benchmark_length = 15
+
     system_info = sys_info()
     print(f"{Color.CYAN}\n-------------------------------------------\n")
     print(f"{system_info[2]}")
     print(f"Operating with {system_info[1]} Cores and {system_info[0]} Threads\n{Color.GREEN}")
+    print(f"Running initial performance test...\n")
+    print(f"The test should ensure that the benchmark length of {benchmark_length} seconds is adjusted to each system.")
+    print(f"It will not go to 100%, it will stop earlier depending on your system.\n")
 
-    best = response = collatz_sequence(1)
-
-    depth = 100000
+    depth = 1000000
+    second_started = False
 
     start_time = time.time()
-    for x in tqdm(range(1, (depth + 1)), desc="Calculating", unit="col"):
+    start_time2 = time.time()
+    num = []
+    for x in tqdm(range(1, (depth + 1)), desc="Testing", unit="col"):
         n = random.randint(1, 100000000000000000)
         response = collatz_sequence(n)
+        if(time.time() - start_time > 0.5):
+            if(second_started == False):
+                second_started = True
+                start_time2 = time.time()
+                num.append(x)
+            if(time.time() - start_time2 > 1):
+                num.append(x)
+                break
+    
+    approx_speed = (num[1] - num[0])
+    print(f"")
 
-        if(response.length > best.length):
-            best = response
+    depth = approx_speed * benchmark_length
+    start_time = time.time()
+    for x in tqdm(range(1, (depth + 1)), desc="Benchmark", unit="col"):
+        n = random.randint(1, 100000000000000000)
+        response = collatz_sequence(n)
     total_time = time.time() - start_time
     speed = depth / total_time
+    
     print(f"\n{Color.RED}Score: {(speed):.2f}")
     print(f"{Color.CYAN}units in collatz sequences/sec | more is better")
 
