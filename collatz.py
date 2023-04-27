75a0b3c2824f25f57502296d28894a14833700d6
Administrator <zivi@plan-g.at>
2023-04-27 15:18:49 +0200

    collatz done

--- collatz.py
+++ collatz.py
@@ -1,5 +1,7 @@
 from tqdm import tqdm
+import cpuinfo
 import random
+import psutil
 import time
 
 # ANSI escape codes for colors
@@ -8,6 +10,13 @@ class Color:
     GREEN = "\033[32m"
     CYAN = "\033[36m"
     RESET = "\033[0m"
+
+# Get CPU stats
+def sys_info(num_threads=None, num_cores=None, cpu_name=None):
+    num_threads = psutil.cpu_count(logical=True)
+    num_cores = psutil.cpu_count(logical=False)
+    cpu_name = cpuinfo.get_cpu_info()['brand_raw']
+    return [num_threads, num_cores, cpu_name]
 
 class CollatzResponse:
     def __init__(self, n, sequence, length):
@@ -33,16 +42,26 @@ def collatz_sequence(n):
     length = len(sequence)
     return CollatzResponse(number, sequence, length)
 
-best = response = collatz_sequence(1)
+if __name__ == "__main__":
+    system_info = sys_info()
+    print(f"{Color.CYAN}\n-------------------------------------------\n")
+    print(f"{system_info[2]}")
+    print(f"Operating with {system_info[1]} Cores and {system_info[0]} Threads\n{Color.GREEN}")
 
-start_time = time.time()
-for x in tqdm(range(1, 100001), desc="Searching", unit="num"):
-    n = random.randint(1, 100000000000000000)
-    response = collatz_sequence(n)
+    best = response = collatz_sequence(1)
 
-    if(response.length > best.length):
-        best = response
-total_time = time.time() - start_time
+    depth = 100000
 
-print(f"\n\n{total_time:.2f} seconds\n")
-print(best)
+    start_time = time.time()
+    for x in tqdm(range(1, (depth + 1)), desc="Calculating", unit="col"):
+        n = random.randint(1, 100000000000000000)
+        response = collatz_sequence(n)
+
+        if(response.length > best.length):
+            best = response
+    total_time = time.time() - start_time
+    speed = depth / total_time
+    print(f"\n{Color.RED}Score: {(speed):.2f}")
+    print(f"{Color.CYAN}units in collatz sequences/sec | more is better")
+
+    print(f"{Color.CYAN}\n-------------------------------------------{Color.RESET}\n")
